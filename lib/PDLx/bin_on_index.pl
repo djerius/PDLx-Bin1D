0514e864ca35ca88d53646c5116c8100991f450d
Diab Jerius <djerius@cfa.harvard.edu>
2016-07-15 16:00:00 -0400

    close over check subref instead of making it package lexical

--- lib/PDLx/bin_on_index.pl
+++ lib/PDLx/bin_on_index.pl
@@ -8,89 +8,92 @@ use Type::Params qw[ compile ];
 use Carp;
 use PDL::Lite;
 
-my $bin_on_index_check;
+{
+    my $check;
 
+    BEGIN {
 
-BEGIN {
-
-    $bin_on_index_check = compile(
-        slurpy Dict [
-            index      => InstanceOf ['PDL'],
-            signal     => Optional   [ InstanceOf ['PDL'] ],
-            nbins      => InstanceOf ['PDL'] | PositiveInt,
-            error      => Optional   [ InstanceOf ['PDL'] | Undef ],
-            error_algo => Optional   [ Enum [ keys %MapErrorAlgo ] | Undef ],
-            oob        => Optional   [ Bool ],
-            offset     => Optional   [Int],
-        ] );
-}
-
-sub bin_on_index {
-
-    my ( $opts ) = $bin_on_index_check->( @_ );
-
-    # specify defaults
-    my %opt = (
-       oob => 0,
-        %$opts,
-    );
-
-    $opt{error_algo} ||=
-        defined $opt{signal} && defined $opt{error} ? 'sdev'
-      : defined $opt{signal}                        ? 'sdev'
-      : defined $opt{error}                         ? 'rss'
-      :                                               'poisson';
-
-    if ( $opt{oob} ) {
-
-	$opt{offset} = 1 unless defined $opt{offset};
-	$opt{nbins} +=2;
+        $check = compile(
+            slurpy Dict [
+                index  => InstanceOf ['PDL'],
+                signal => Optional   [ InstanceOf ['PDL'] ],
+                nbins  => InstanceOf ['PDL'] | PositiveInt,
+                error  => Optional   [ InstanceOf ['PDL'] | Undef ],
+                error_algo => Optional [ Enum [ keys %MapErrorAlgo ] | Undef ],
+                oob        => Optional [Bool],
+                offset     => Optional [Int],
+            ] );
     }
 
-    $opt{offset} = 0 unless defined $opt{offset};
 
-    croak( "must specify error attribute if 'rss' errors selected\n" )
-      if !defined $opt{error} && $opt{error_algo} eq 'rss';
+    sub bin_on_index {
 
-    $opt{flags}
-      = ( ( defined $opt{signal}      && BIN_ARG_HAVE_SIGNAL ) || 0 )
-      | ( ( defined $opt{error}       && BIN_ARG_HAVE_ERROR  ) || 0 )
-      | ( ( $opt{set_bad}             && BIN_ARG_SET_BAD     ) || 0 )
-      | ( ( $opt{oob}                 && BIN_ARG_SAVE_OOB    ) || 0 )
-      | $MapErrorAlgo{ $opt{error_algo} };
+        my ( $opts ) = $check->( @_ );
 
-    $opt{maxnbins} = PDL::Core::topdl( $opt{nbins} )->max;
+        # specify defaults
+        my %opt = (
+            oob => 0,
+            %$opts,
+        );
 
-    my @pin   = qw[ signal index error nbins ];
-    my @pout  = qw[ nelem b_signal b_error b_mean b_min b_max ];
-    my @ptmp  = qw[ b_error2 b_m2 b_weight  ];
-    my @oargs = qw[ flags maxnbins offset ];
+        $opt{error_algo} ||=
+            defined $opt{signal} && defined $opt{error} ? 'sdev'
+          : defined $opt{signal} ? 'sdev'
+          : defined $opt{error}  ? 'rss'
+          :                        'poisson';
+
+        if ( $opt{oob} ) {
+
+            $opt{offset} = 1 unless defined $opt{offset};
+            $opt{nbins} += 2;
+        }
+
+        $opt{offset} = 0 unless defined $opt{offset};
+
+        croak( "must specify error attribute if 'rss' errors selected\n" )
+          if !defined $opt{error} && $opt{error_algo} eq 'rss';
+
+        $opt{flags}
+          = ( ( defined $opt{signal} && BIN_ARG_HAVE_SIGNAL ) || 0 )
+          | ( ( defined $opt{error}  && BIN_ARG_HAVE_ERROR )  || 0 )
+          | ( ( $opt{set_bad}        && BIN_ARG_SET_BAD )     || 0 )
+          | ( ( $opt{oob}            && BIN_ARG_SAVE_OOB )    || 0 )
+          | $MapErrorAlgo{ $opt{error_algo} };
+
+        $opt{maxnbins} = PDL::Core::topdl( $opt{nbins} )->max;
+
+        my @pin   = qw[ signal index error nbins ];
+        my @pout  = qw[ nelem b_signal b_error b_mean b_min b_max ];
+        my @ptmp  = qw[ b_error2 b_m2 b_weight  ];
+        my @oargs = qw[ flags maxnbins offset ];
 
 
-    # several of the input piddles are optional.  the PP routine
-    # doesn't know that and will complain about the wrong number of
-    # dimensions if we pass a null piddle. A 1D zero element piddle
-    # will have its dimensions auto-expanded without much
-    # wasted memory.
-    $opt{signal} = PDL->zeroes( PDL::double, 0 ) if !defined $opt{signal};
+        # several of the input piddles are optional.  the PP routine
+        # doesn't know that and will complain about the wrong number of
+        # dimensions if we pass a null piddle. A 1D zero element piddle
+        # will have its dimensions auto-expanded without much
+        # wasted memory.
+        $opt{signal} = PDL->zeroes( PDL::double, 0 ) if !defined $opt{signal};
 
-    # make sure we use the same type as the input signal
-    $opt{$_} = $opt{signal}->zeroes( 1 ) for grep { !defined $opt{$_} } @pin;
+        # make sure we use the same type as the input signal
+        $opt{$_} = $opt{signal}->zeroes( 1 )
+          for grep { !defined $opt{$_} } @pin;
 
-    $opt{$_} = PDL->null for grep { !defined $opt{$_} } @pout;
-    $opt{$_} = PDL->null for grep { !defined $opt{$_} } @ptmp;
+        $opt{$_} = PDL->null for grep { !defined $opt{$_} } @pout;
+        $opt{$_} = PDL->null for grep { !defined $opt{$_} } @ptmp;
 
-    _bin_on_index_int( @opt{ @pin, @pout, @ptmp, @oargs } );
+        _bin_on_index_int( @opt{ @pin, @pout, @ptmp, @oargs } );
 
-    my %results = map {
-        ( my $nkey = $_ ) =~ s/^b_//;
-        $nkey, $opt{$_}
-    } @pout;
+        my %results = map {
+            ( my $nkey = $_ ) =~ s/^b_//;
+            $nkey, $opt{$_}
+        } @pout;
 
 
-    return \%results;
+        return \%results;
+    }
+
 }
-
 
 =pod
 
